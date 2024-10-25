e4ccc49bad2869266d42d54813bb3d1ac029e639
har2525life <har2525life@outlook.com>
2024-10-26 07:20:48 +0900

    fix

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -28,7 +28,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>          
         <meta charSet="utf-8" />
         <meta name="viewport" 
-        content="width=device-width, initial-scale=1" />
+        content="width=device-width, 
+        
+        
+        
+        
+        
+        
+        initial-scale=1" />
         <Meta />
         <Links />
       </head>
