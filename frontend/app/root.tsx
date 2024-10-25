811f202b595e821cf90d3234fa4d2575725db761
har2525life <har2525life@outlook.com>
2024-10-26 07:37:14 +0900

    fix

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -23,8 +23,12 @@ export const links: LinksFunction = () => [
 
 
 
+
+    href: "https://fonts.googleapis.com/c
     
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    
+    
+    ss2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
 
