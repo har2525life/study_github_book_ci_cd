6d1e3d8bd8a87d835b46c36ee2579a278ddae865
har2525life <har2525life@outlook.com>
2024-10-26 07:08:25 +0900

    fix: test

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -25,9 +25,17 @@ export const links: LinksFunction = () => [
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
+      <head>          
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" 
+        
+        
+        
+        
+        
+        
+        
+        content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
